33574b209b982a80183c817dcd7bbe500d533d1b
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-08-18 13:03:50 +0300

    complete deliverables

--- computer.rb
+++ computer.rb
@@ -2,6 +2,7 @@
 class Computer
     attr_Accessor :model, :country, :year
     
+    
     def initialize(model, country, year)
         @model = model
         @country = country
@@ -15,5 +16,17 @@ class Computer
 end
 
 # TODO: Implement class that inherits from Computer class
+class Hp < Computer
+end
+
+comp1 = Hp.new("Envy", "USA", 2022)
+comp1.print_computer
+puts Hp.show_memory
+
+class MacBook < Computer
+end
+
+comp2 = MacBook.new("PRO M1", "Year", 2020)
+comp2.print_computer
 
 # TODO: Use the super keyword to access attributes and methods from Computer
--- plant.rb
+++ plant.rb
@@ -1,1 +1,34 @@
 # TODO: Implement a class do illustrate how class variables and methods work.
+class Plant
+
+    @@num_of_trees = 0
+
+    attr_accessor :species
+
+    def initialize(species)
+        @species = species
+        @@num_of_trees += 1
+    end
+
+    def speak_of_species
+        puts "#{self.species} is speaking"
+    end
+
+    def self.tree_count
+        @@num_of_trees
+    end
+
+end
+
+#ACCESS CLASS VARIABLE
+
+mwarubaini = Plant.new("Neem Tree")
+mwarubaini.speak_of_species
+puts Plant.tree_count
+
+eucalyptus = Plant.new("EWucalyptus Tree")
+mango = Plant.new("Mango Tree")
+orange = Plant.new("Orange Tree")
+cypress = Plant.new("Cypress Tree")
+
+puts Plant.tree_count
